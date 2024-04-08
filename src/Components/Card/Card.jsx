eb9dd6d5950d92b9409a0236e1da80fb9ce4351c
devsafi <mdkawserferdoussafi@gmail.com>
2024-04-08 22:25:35 +0600

    slider title added

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -8,7 +8,7 @@ const Card = ({ cardData }) => {
 
     return (
         <div className="mt-20">
-            <div className="text-center animate__animated animate__fadeInLeft text-4xl font-bold">
+            <div className="text-center animate__animated animate__bounceInUp text-4xl font-bold">
                 Estates
             </div>
             <div className="grid mt-10 gap-7 grid-cols-1 md:grid-cols-1 lg:grid-cols-3">
--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -6,10 +6,9 @@ import { AuthContext } from "../../../Context/ContextProvider";
 const Navbar = () => {
 
     const { user, LogOut } = useContext(AuthContext)
-    console.log(user);
     const links = <>
 
-        <li className="hover:scale-110 transition"><NavLink style={({ isActive }) => {
+        <li className="hover:scale-110 transition animate__animated animate__backInRight"><NavLink style={({ isActive }) => {
             return {
                 fontWeight: isActive ? "bold" : "",
                 color: isActive ? "blue" : "",
@@ -17,7 +16,7 @@ const Navbar = () => {
             };
         }} to="/">Home</NavLink></li>
 
-        <li className="hover:scale-110 transition"><NavLink style={({ isActive }) => {
+        <li className="hover:scale-110 transition animate__animated animate__backInRight"><NavLink style={({ isActive }) => {
             return {
                 fontWeight: isActive ? "bold" : "",
                 color: isActive ? "blue" : "",
@@ -25,7 +24,7 @@ const Navbar = () => {
             };
         }} to="/about">About</NavLink></li>
 
-        <li className="hover:scale-110 transition lg:flex hidden"><NavLink style={({ isActive }) => {
+        <li className="hover:scale-110 transition animate__animated animate__backInRight lg:flex hidden"><NavLink style={({ isActive }) => {
             return {
                 fontWeight: isActive ? "bold" : "",
                 color: isActive ? "blue" : "",
@@ -33,7 +32,7 @@ const Navbar = () => {
             };
         }} to="/login">Login</NavLink></li>
 
-        <li className="hover:scale-110 transition "><NavLink style={({ isActive }) => {
+        <li className="hover:scale-110 transition animate__animated animate__backInRight"><NavLink style={({ isActive }) => {
             return {
                 fontWeight: isActive ? "bold" : "",
                 color: isActive ? "blue" : "",
@@ -47,7 +46,7 @@ const Navbar = () => {
     return (
         <div className="navbar bg-base-100 mt-2 lg:mt-5 container mx-2 md:mx-5 lg:mx-auto">
             <div className="flex-1 hidden lg:flex ">
-                <Link to="/" className="text-4xl animate__animated animate__flipInX text-blue-500 font-bold"><span className="text-red-500">Luxuria</span>Luxe</Link>
+                <Link to="/" className="text-4xl animate__animated animate__backInLeft text-blue-500 font-bold"><span className="text-red-500">Luxuria</span>Luxe</Link>
             </div>
             <div className="flex-1 lg:hidden">
                 <Link to="/" className="text-2xl text-blue-500 font-bold"><span className="text-red-500">L</span>L</Link>
@@ -68,7 +67,7 @@ const Navbar = () => {
                             </div>
                         </div>
                         <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                            <li className="text-xl font-semibold"><a>User name: {user.displayName || "Name not found"}</a></li>
+                            <li className="text-xl font-semibold"><a>User name: {user?.displayName || "Name not found"}</a></li>
                             <li className="font-semibold text-xl" onClick={() => LogOut()}><a>Logout</a></li>
                         </ul>
                     </div> : ""
